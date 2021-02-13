de86ec7f0fb73d193838d29aed4d1e3b7a8d851a
Srinivas M <srinivasarao.m@everest.engineering>
2021-02-13 17:47:03 +0530

    Testing aws pipeline

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Learning AWS Code Deploy, Code Pipeline!'));
+app.get('/', (req, res) => res.send('Learning AWS Code Deploy, Code Pipeline!. Testing Full pipeline!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
